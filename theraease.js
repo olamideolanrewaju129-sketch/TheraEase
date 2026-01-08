823727ebdac047a438ae837d8573a5222f40366f
Dauda Mariam <olamideolanrewaju129@gmail.com>
2026-01-08 13:23:06 +0100

    good

--- theraease.js
+++ theraease.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
    OTHER ANIMATIONS & UTILS
    ========================== */
 
-const elements = document.querySelectorAll('.fade-in, .animate-down, .animate-up, .slide-in-left');
+const elements = document.querySelectorAll('.fade-in, .animate-down, .animate-up, .slide-in-left, .slide-in-right');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
